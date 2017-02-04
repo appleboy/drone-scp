ff3916db1b9d1d4abb46ce0ea43af036823d3bf1
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-02-04 20:35:57 +0800

    test: add easyssh scp and ssh command testing.

--- easyssh/easyssh_test.go
+++ easyssh/easyssh_test.go
@@ -1,6 +1,8 @@
 package easyssh
 
 import (
+	"os"
+	"os/user"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -19,4 +21,39 @@ func TestGetKeyFile(t *testing.T) {
 
 	_, err = getKeyFile("../tests/.ssh/id_rsa")
 	assert.NoError(t, err)
+}
+
+func TestRunCommand(t *testing.T) {
+	ssh := &MakeConfig{
+		Server:  "localhost",
+		User:    "drone-scp",
+		Port:    "22",
+		KeyPath: "../tests/.ssh/id_rsa",
+	}
+
+	output, err := ssh.Run("whoami")
+	assert.Equal(t, "drone-scp\n", output)
+	assert.NoError(t, err)
+}
+
+func TestSCPCommand(t *testing.T) {
+	ssh := &MakeConfig{
+		Server:  "localhost",
+		User:    "drone-scp",
+		Port:    "22",
+		KeyPath: "../tests/.ssh/id_rsa",
+	}
+
+	err := ssh.Scp("../tests/a.txt")
+	assert.NoError(t, err)
+
+	u, err := user.Lookup("drone-scp")
+	if err != nil {
+		t.Fatalf("Lookup: %v", err)
+	}
+
+	// check file exist
+	if _, err := os.Stat(u.HomeDir + "/a.txt"); os.IsNotExist(err) {
+		t.Fatalf("SCP-error: %v", err)
+	}
 }
